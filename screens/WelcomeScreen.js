a0a5f938f585e44cabd54c4096c16078cd3f8a50
ravi <ravivishwakarma1590@gmail.com>
2022-05-25 15:56:44 +0530

    my first commit

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,29 @@
+
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function WelcomeScreen() {
+  return (
+    <View style={styles.rootContainer}>
+      <Text style={styles.title}>Welcome!</Text>
+    </View>
+  );
+}
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 32,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+});
+ 
